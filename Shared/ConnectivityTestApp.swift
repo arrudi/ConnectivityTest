2967096719d554f1d2a13a45a1f1d689e815592a
Felipe Arrudi <felipe.arrudi@gmail.com>
2021-12-01 19:14:23 +0000

    Initial Commit

---
+++ Shared/ConnectivityTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectivityTestApp.swift
+//  Shared
+//
+//  Created by A337950 on 01/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct ConnectivityTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
